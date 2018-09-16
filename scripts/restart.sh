a348031dd78f61a843c63c57e2e8a0b537c45d4a
nakabonne <rodriguez.nak@gmail.com>
2018-09-16 11:34:59 +0900

    add h2o syntax check

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -8,7 +8,7 @@ sudo cp etc/h2o.conf /etc/h2o/h2o.conf
 cd /home/isucon/torb/webapp/go
 make build
 
-## TODO: h2o.confのシンタックスチェック
+sudo /usr/sbin/h2o -t -c /etc/h2o/h2o.conf
 sudo systemctl restart h2o
 
 sudo systemctl restart torb.go
