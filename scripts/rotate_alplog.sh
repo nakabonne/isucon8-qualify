adbf463af33923046be4970ea5007640dcc4b000
nakabonne <rodriguez.nak@gmail.com>
2018-09-16 11:09:16 +0900

    fix rotate

--- scripts/rotate_alplog.sh
+++ scripts/rotate_alplog.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-ALP_LOG_FILE=/var/log/nginx/access.log
+ALP_LOG_FILE=/var/log/h2o/access.log
 ROTATE_ALP_LOG_DIR="$HOME/alp"
 ROTATE_ALP_LOG_FILE="$ROTATE_ALP_LOG_DIR/alp-$(date '+%Y%m%d%H%M%S').log"
 
