332bbea3128813ae380f128207e538e9f0390d8a
nakabonne <rodriguez.nak@gmail.com>
2018-09-22 16:48:46 +0900

    update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,16 +2,32 @@
 set -e
 
 git pull
+echo "$(tput setaf 2)Pull Succeeded! $(tput sgr0)"
+echo ""
 
+# reverse proxy
 sudo cp etc/h2o.conf /etc/h2o/h2o.conf
 
+# log rotate
+make rotate
+make mysqldumpslow
+echo "$(tput setaf 2)log rotate Succeeded! $(tput sgr0)"
+echo ""
+
+# app
 cd /home/isucon/torb/webapp/go
 make build
 
 sudo /usr/sbin/h2o -t -c /etc/h2o/h2o.conf
 sudo systemctl restart h2o
+echo "$(tput setaf 2)reverse proxy reload Succeeded! $(tput sgr0)"
+echo ""
 
 sudo systemctl restart torb.go
-
+echo "$(tput setaf 2)application reload Succeeded! $(tput sgr0)"
 echo ""
-echo "$(tput setaf 2)Restart Succeeded!!! ✔︎$(tput sgr0)"
+
+echo "$(tput setaf 2)############################$(tput sgr0)"
+echo "$(tput setaf 2)## Restart Succeeded!!! ✔︎ ##$(tput sgr0)"
+echo "$(tput setaf 2)############################$(tput sgr0)"
+echo ""
