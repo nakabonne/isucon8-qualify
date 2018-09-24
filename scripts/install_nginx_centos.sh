b1d54c7b62d85e203a46c74895329ded6dceb227
nakabonne <rodriguez.nak@gmail.com>
2018-09-24 11:31:23 +0900

    add chenge mode command

--- scripts/install_nginx_centos.sh
+++ scripts/install_nginx_centos.sh
@@ -2,8 +2,9 @@
 set -e
 
 sudo touch /etc/yum.repos.d/nginx.repo
+sudo chmod 664 /etc/yum.repos.d/nginx.repo
 
-suco cat > /etc/yum.repos.d/nginx.repo << "EOF"
+cat > /etc/yum.repos.d/nginx.repo << "EOF"
 [nginx]
 name=nginx repo
 baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/
