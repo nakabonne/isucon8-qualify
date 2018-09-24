0582681873fb9f9c68514038f0378ddf51347910
nakabonne <rodriguez.nak@gmail.com>
2018-09-24 11:27:55 +0900

    add sudo

--- scripts/install_nginx_centos.sh
+++ scripts/install_nginx_centos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-touch /etc/yum.repos.d/nginx.repo
+sudo touch /etc/yum.repos.d/nginx.repo
 
-cat > /etc/yum.repos.d/nginx.repo << "EOF"
+suco cat > /etc/yum.repos.d/nginx.repo << "EOF"
 [nginx]
 name=nginx repo
 baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/
